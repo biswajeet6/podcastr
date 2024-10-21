40e2340da0d58160049612d296128f42635c49a7
biswajeet6 <biswajeetjena921@gmail.com>
2024-10-21 11:22:51 +0530

    commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://causal-jackal-69.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
